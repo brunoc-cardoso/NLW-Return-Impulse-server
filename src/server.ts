1a32998d63f7b5ab89b0f1f05e17a75626c660ad
Bruno Cardoso <brunocpessoal@gmail.com>
2022-05-05 15:01:26 -0300

    add prisma

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const PORT = process.env.PORT || 3333;
 const server = express();
 
 server.use(express.json());
+
+server.get('/users', (request: Request, response: Response) => {
+  return response.send('Hello World');
+});
 
 server.listen(PORT, () => {
   console.log(`🔥 Server is running on port ${PORT}`);
