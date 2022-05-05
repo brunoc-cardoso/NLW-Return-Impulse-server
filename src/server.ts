037357244dfa0561e2be41a9569c66c8e5792991
Bruno Cardoso <brunocpessoal@gmail.com>
2022-05-05 15:45:53 -0300

    add post route for feedbacks

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,24 @@
 import express, { Request, Response } from 'express';
 
+import { prisma } from './config/prisma';
+
 const PORT = process.env.PORT || 3333;
 const server = express();
 
 server.use(express.json());
 
-server.get('/users', (request: Request, response: Response) => {
-  return response.send('Hello World');
+server.post('/feedbacks', async (request: Request, response: Response) => {
+  const { type, comment, screenshot } = request.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot,
+    },
+  });
+
+  return response.status(201).json({ data: feedback });
 });
 
 server.listen(PORT, () => {
